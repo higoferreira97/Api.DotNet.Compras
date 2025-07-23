d3dca905c5ec6aceb7ac664eae16e0a798ba0610
higoferreira97 <higoferreira.dev@gmail.com>
2025-07-22 21:25:28 -0300

    Feat: Criado as classes person e DomainValidationException, A classe Person representa uma pessoa com nome, documento e telefone, e só deixa ser criada se os dados forem válidos, usando a classe DomainValidationException( ) para garantir isso.

---
+++ Api.DotNet.Domain/Entities/Person.cs
@@ -0,0 +1,38 @@
+﻿
+
+using Api.DotNet.Domain.Validations;
+
+namespace Api.DotNet.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string name, string document, string phone)
+        {
+            Validation(name, document, phone);
+        }
+
+        public Person(int id, string name, string document, string phone)
+        {
+            DomainValidationException.when(id < 0, "Id deve ser maior que zero");
+            Id = id;
+            Validation(name, document, phone);
+        }
+
+        private void Validation(string name, string document, string phone)
+        {
+            DomainValidationException.when(string.IsNullOrEmpty(name), "Nome deve ser informado!");
+            DomainValidationException.when(string.IsNullOrEmpty(document), "Documento deve ser informado!");
+            DomainValidationException.when(string.IsNullOrEmpty(phone), "Celular deve ser informado!");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+
+    }
+}
